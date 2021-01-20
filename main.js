dfaca2ef2a38c114a1b7a5b5a2f3dba6afbe92b6
TejuDevz <75902941+TejuDevz@users.noreply.github.com>
2021-01-20 18:00:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-function preload() {}
+noseX = 0;
+noseY = 0;
+
+function preload() {
+  img = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
+  img2 = loadImage("https://i.postimg.cc/PxFvYgkv/l1.png");
+}
 
 function setup() {
   canvas = createCanvas(300, 300);
@@ -17,14 +23,15 @@ function modelLoaded() {
 
 function gotPoses(results) {
   if (results.length > 0) {
-    console.log(results);
-    console.log("pose nose X = " + results[0].pose.nose.x);
-    console.log("pose nose Y = " + results[0].pose.nose.y);
+    noseX = results[0].pose.nose.x - 14;
+    noseY = results[0].pose.nose.y + 2;
   }
 }
 
 function draw() {
   image(video, 0, 0, 300, 300);
+  image(img, noseX, noseY, 30, 30);
+  image(img2, noseX, noseY + 15, 30, 30);
 }
 
 function take_snapshot() {
